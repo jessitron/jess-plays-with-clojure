ece8bd500dda692adfb45eac9b3b107165c5444a
Jessica Kerr <jessitron@outpace.com>
2014-05-29 17:16:13 -0500

    Now the failing test is my test

--- test/play/core_test.clj
+++ test/play/core_test.clj
@@ -2,6 +2,23 @@
   (:require [clojure.test :refer :all]
             [play.core :refer :all]))
 
-(deftest a-test
+(def eggs {:food "eggs" :meal "breakfast"})
+(def steak {:food "steak" :meal "lunch"})
+(def pizza {:food "pizza" :meal "lunch"})
+
+(defn sortify [instructions input] input)
+
+
+(deftest test-sort-according-to-instructions
+  (testing "the sample in the blog"
+    (let [instructions [[:meal :asc {"breakfast" 1 "lunch" 2 "dinner" 3}]
+                        [:food :desc]]
+          input [ pizza steak eggs ]
+          expected [eggs steak pizza]
+          result (sortify instructions input)]
+      (is (= result expected))))
+  )
+
+(Deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
